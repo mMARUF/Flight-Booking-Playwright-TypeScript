5ca12e1a1969a0b00762b334d4fd525f73850a28
Maruf Rahman <maruf.rahman.95@gmail.com>
2025-06-18 19:28:43 +0600

    Created the Project

---
+++ tests/flight-price-comparison.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+
+test.describe('Flight Booking Tests', () => {
+
+  test('Flight Price Comparison between 2 Airlines', async ({ page }) => {
+
+    
+  });
+
+});
